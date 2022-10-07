ce7d1e13bf5202ef4e4192a9bc0b63c400f24f9c
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-10-08 02:59:47 +0530

    extract all tasks, list employee API

--- api/getAllTasks.js
+++ api/getAllTasks.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql')
 
-async function getAll (req, res) {
+async function getAllTasks (req, res) {
   const con = mysql.createConnection({
     host: 'localhost',
     user: 'pico',
@@ -8,22 +8,29 @@ async function getAll (req, res) {
     database: 'picoperformance'
   })
 
+  req = req.body
+
+  const find = req.EmailId
   con.connect((err) => {
     if (err) {
       res.status(400).send('Datbase Error')
       return 0
     }
     console.log('Connected!')
-    const sql = 'SELECT * FROM Employees WHERE isAdmin = 0'
+    const sql = 'SELECT * FROM Employee WHERE EmailId = "' + find + '"'
+
     con.query(sql, function (erro, result) {
       if (erro) {
         res.status(400).send('Unknown Error')
         return 0
       }
+      if (sql.length === 0) {
+        res.status(404).send('No record in database')
+      }
       console.log('Number of records inserted: ' + result.affectedRows)
-      res.send(sql)
+      res.send(result)
     })
   })
 }
 
-exports.execute(getAll)
+exports.execute(getAllTasks)
