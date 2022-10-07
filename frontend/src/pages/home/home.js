9e3390aae367b1889afd5a25c1b2c615a1a85173
Gourav1100 <gauravb1100@gmail.com>
2022-10-07 23:40:50 +0530

    added 404 page

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Footer from "../../components/footer/footer";
+import Header from "../../components/header/header";
+
+import styles from "./home.module.css";
 
 export default class Home extends React.Component {
     render() {
         return (
             <>
-                This is home page
+                <Header />
+                <div className={styles.container}>
+                    This is home
+                </div>
+                <Footer />
             </>
         );
     }
--- frontend/src/pages/nopage/nopage.js
+++ frontend/src/pages/nopage/nopage.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import styles from "./nopage.module.css";
+import logo from "../../icons/error-404.png";
 
 export default class NoPage extends React.Component {
     render() {
         return (
-            <>
-            Nothing here!!!
-            </>
+            <div style={{flexGrow: 1}}>
+            <Grid className={styles.nopageContainer} container spacing={5} style={{ display: 'flex' }} alignContent={"center"} alignItems={"center"}>
+                <Grid item xs={12} sm={6} textAlign={"right"}>
+                    <img src={logo} height = "200vh" width = "auto" alt = "404" />
+                </Grid>
+                <Grid className={styles.messageConatiner} item xs={12} sm={6} textAlign={"left"}>
+                    Oops, <br />page not found.
+                </Grid>
+            </Grid>
+            </div>
         );
     }
 }
