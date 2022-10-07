a2c86ba86cf7ebf34905836ac8e1d30716ff7b3a
TejaswiBishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-10-08 02:56:58 +0530

    addtask updated with constraints

--- api/addtask.js
+++ api/addtask.js
@@ -8,6 +8,19 @@ function handler(req, res) {
     });
     req = req.body
     const Tasks = [[req.EmployeeId, req.TaskDescription, req.StartDate, req.TimeTaken, req.TaskType]]
+    var comp = req.StartDate.slice(0, 10);
+    const d = new Date();
+    var mnth = d.getMonth() + 1;
+    mnth = mnth.toString();
+    if (mnth.length == 1) mnth = '0' + mnth;
+    dayy = d.getDate().toString();
+    if (dayy.length == 1) dayy = '0' + dayy;
+    var comp2 = d.getFullYear() + '-' + mnth + '-' + dayy
+    console.log(comp2);
+    if (comp > comp2){
+        res.status(400).send('Date Value More than Current Date')
+        return 0;
+    }
     con.connect(async (err)=>{
         if (err) {
             res.status(400).send("Datbase Error");
