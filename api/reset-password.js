90aa4ac6ef26e4d199bd4be004be63e5ad5acf7e
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-10-08 16:51:45 +0530

    added token drop and made token table

--- api/reset-password.js
+++ api/reset-password.js
@@ -34,6 +34,15 @@ function updatePassword (req, res) {
 
       console.log('Password updated successfully')
     })
+
+    con.query('DELETE FROM Tokens WHERE Token = "' + token + '"', function (err, result) {
+      if (err) throw err
+      if (result.affectedRows === 0) {
+        res.status(400).send('Query failed')
+      }
+
+      console.log('Token dropped successfully')
+    })
   })
 }
 
--- database/Schema.sql
+++ database/Schema.sql
@@ -8,7 +8,6 @@ CREATE TABLE Employee(
     PasswordHash VARCHAR(128),
     IsBlocked BOOL DEFAULT FALSE,
     IsAdmin BOOL DEFAULT FALSE,
-    Token VARCHAR(100),
     PRIMARY KEY(EmployeeId)
 );
 
@@ -22,5 +21,12 @@ CREATE TABLE Tasks(
     CONSTRAINT PK1 PRIMARY KEY(TaskId, EmployeeId),
     CONSTRAINT FK1 FOREIGN KEY(EmployeeId) REFERENCES Employee(EmployeeId)
 );
+
+CREATE TABLE Tokens(
+    EmployeeId int,
+    EmailId VARCHAR(100) UNIQUE,
+    Token VARCHAR(50)
+);
+
 drop TABLE Tasks;
 drop table Employee;
