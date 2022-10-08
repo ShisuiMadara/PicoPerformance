4dd19bde7b58ebf363cd3193ac1e4f60487217ec
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-10-08 14:14:56 +0530

    added email functionality

--- api/forgotPassword.js
+++ api/forgotPassword.js
@@ -1,8 +1,109 @@
 const bcrypt = require('bcrypt')
 const mysql = require('mysql')
+const nodemailer = require('nodemailer')
+const randtoken = require('rand-token')
 
 async function hashPassword (Password) {
-  return await bcrypt.hash(Password, 10)
+  return await bcrypt.hash(Password)
+}
+
+async function sendEmail (email, token) {
+  const mail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EmailUser,
+      pass: process.env.EmailPassword
+    }
+  })
+
+  const mailOptions = {
+    from: 'picoPerformance@gmail.com',
+    to: email,
+    subject: 'Reset Password Link - picoPerformance',
+    html: '<p>You requested for reset password, kindly use this <a href="http://localhost:4000/reset-password?token=' + token + '">link</a> to reset your password</p>'
+
+  }
+
+  await mail.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log('Error sending mail')
+    }
+    console.log(info)
+  })
+}
+
+function resetPassword (req, res) {
+  const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'pico',
+    password: 'password',
+    database: 'picoperformance'
+  })
+
+  req = req.body
+  const email = req.EmailId
+
+  con.query('SELECT * FROM Employee WHERE EmailId ="' + email + '"', function (err, result) {
+    if (err) throw err
+
+    let type = ''
+    let msg = ''
+
+    console.log(result[0])
+
+    if (result[0].email.length > 0) {
+      const token = randtoken.generate(20)
+
+      const sent = sendEmail(email, token)
+
+      if (sent !== '0') {
+        const data = {
+          token
+        }
+
+        con.query('UPDATE users SET ? WHERE email ="' + email + '"', data, function (err, result) {
+          if (err) throw err
+          res.write(result)
+        })
+
+        type = 'success'
+        msg = 'The reset password link has been sent to your email address'
+      } else {
+        type = 'error'
+        msg = 'Something goes to wrong. Please try again'
+      }
+    } else {
+      console.log('2')
+      type = 'error'
+      msg = 'The Email is not registered with us'
+    }
+
+    req.flash(type, msg)
+    res.redirect('/')
+    res.send()
+  })
+}
+
+function updatePassword (req, res) {
+  const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'pico',
+    password: 'password',
+    database: 'picoperformance'
+  })
+
+  req = req.body
+
+  const token = req.body.token
+  const password = req.body.password
+
+  con.query('SELECT * FROM Employee WHERE token ="' + token + '"', function (err, result) {
+    if (err) throw err
+
+    const PasswordHash = hashPassword(password)
+
+    
+  })
 }
 
 function forgotPassword (req, res) {
