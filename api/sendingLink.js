6072df86439680d037ceb730f77d277a47709259
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-10-09 02:26:40 +0530

    added mail gun API

--- api/sendingLink.js
+++ api/sendingLink.js
@@ -5,47 +5,47 @@ const mg = require('nodemailer-mailgun-transport')
 
 async function sendEmail (email, token) {
   // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
-  const auth = {
-    auth: {
-      api_key: '5ec105ebff13bd8e38ff68b3067b575e-381f2624-44ec0801',
-      domain: 'postmaster@sandboxdeed5c321f4b4978a28591a344e75fca.mailgun.org'
-    }
-  }
-
-  const nodemailerMailgun = nodemailer.createTransport(mg(auth))
-
-  nodemailerMailgun.sendMail({
-    from: '2020ucs0090@iitjammu.ac.in',
-    to: email,
-    subject: 'Reset Password Link - picoPerformance',
-    html: '<p>You requested for reset password, kindly use this <a href="http://localhost:5000/reset-password?token=' + token + '">link</a> to reset your password</p>'
-  }, function (err, info) {
-    if (err) {
-      console.log('Error: ' + err)
-    } else {
-      console.log('Response: ' + info)
-    }
-  })
-
-  // const transporter = nodemailer.createTransport({
-  //   host: 'smtp.mailgun.org',
-  //   port: 587,
+  // const auth = {
   //   auth: {
-  //     user: 'postmaster@sandboxdeed5c321f4b4978a28591a344e75fca.mailgun.org',
-  //     pass: '1d44e38354a64d4c0a4c6da9ccbab793-381f2624-5d0adc01'
+  //     api_key: '5ec105ebff13bd8e38ff68b3067b575e-381f2624-44ec0801',
+  //     domain: 'postmaster@sandboxdeed5c321f4b4978a28591a344e75fca.mailgun.org'
   //   }
-  // })
+  // }
 
-  // const info = await transporter.sendMail({
-  //   from: 'picoPerformance@gmail.com',
+  // const nodemailerMailgun = nodemailer.createTransport(mg(auth))
+
+  // nodemailerMailgun.sendMail({
+  //   from: '2020ucs0090@iitjammu.ac.in',
   //   to: email,
   //   subject: 'Reset Password Link - picoPerformance',
   //   html: '<p>You requested for reset password, kindly use this <a href="http://localhost:5000/reset-password?token=' + token + '">link</a> to reset your password</p>'
+  // }, function (err, info) {
+  //   if (err) {
+  //     console.log('Error: ' + err)
+  //   } else {
+  //     console.log('Response: ' + info)
+  //   }
   // })
 
-  // console.log('Message sent: %s', info.messageId)
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mailgun.org',
+    port: 587,
+    auth: {
+      user: 'postmaster@sandboxdeed5c321f4b4978a28591a344e75fca.mailgun.org',
+      pass: '1d44e38354a64d4c0a4c6da9ccbab793-381f2624-5d0adc01'
+    }
+  })
 
-  // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+  const info = await transporter.sendMail({
+    from: 'picoPerformance@gmail.com',
+    to: email,
+    subject: 'Reset Password Link - picoPerformance',
+    html: '<p>You requested for reset password, kindly use this <a href="http://localhost:5000/reset-password?token=' + token + '">link</a> to reset your password</p>'
+  })
+
+  console.log('Message sent: %s', info.messageId)
+
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
 
   // const mailgun = require('mailgun-js')
   // const DOMAIN = 'sandboxdeed5c321f4b4978a28591a344e75fca.mailgun.org'
