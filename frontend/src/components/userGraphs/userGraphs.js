fbd1f5f395baa1a3821bd51dcfbfbf23160594a2
Tejaswi Bishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-10-10 17:57:13 +0530

    completed graphs.

--- frontend/src/components/userGraphs/userGraphs.js
+++ frontend/src/components/userGraphs/userGraphs.js
@@ -1,7 +1,7 @@
 import { Grid } from '@mui/material';
 import react from 'react';
 import axios from 'axios'
-import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import { PieChart, Pie, Cell, Tooltip, Legend, BarChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from "recharts";
 import styles from './userGraphs.module.css';
 
 class PieChartToday extends react.Component{
@@ -193,67 +193,85 @@ class StackedChart extends react.Component{
     }
     render(){
         const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
+        const dx = this.props.data;
         const chartData = [
             {
-                "name" : "Break",
-                "value" : this.props.data.Break.Sum == null ? 0 : this.props.data.Break.Sum
+                name: "Day 1",
+                Break: dx[0][0].Sum,
+                Meeting : dx[0][1].Sum,
+                Work: dx[0][2].Sum
             },
             {
-                "name" : "Meeting",
-                "value" : this.props.data.Meeting.Sum == null ? 0 : this.props.data.Meeting.Sum
-            },
-            {
-                "name" : "Work",
-                "value" : this.props.data.Work.Sum == null ? 0 : this.props.data.Work.Sum
+                name: "Day 2",
+                Break: dx[1][0].Sum,
+                Meeting : dx[1][1].Sum,
+                Work: dx[1][2].Sum
+            },{
+                name: "Day 3",
+                Break: dx[2][0].Sum,
+                Meeting : dx[2][1].Sum,
+                Work: dx[2][2].Sum
+            },{
+                name: "Day 4",
+                Break: dx[3][0].Sum,
+                Meeting : dx[3][1].Sum,
+                Work: dx[3][2].Sum
+            },{
+                name: "Day 5",
+                Break: dx[4][0].Sum,
+                Meeting : dx[4][1].Sum,
+                Work: dx[4][2].Sum
+            },{
+                name: "Day 6",
+                Break: dx[5][0].Sum,
+                Meeting : dx[5][1].Sum,
+                Work: dx[5][2].Sum
+            },{
+                name: "Day 7",
+                Break: dx[6][0].Sum,
+                Meeting : dx[6][1].Sum,
+                Work: dx[6][2].Sum
             }
         ]
-        const tooltipData=[
-            {
-                "name" : "Break Time",
-                "value" : this.props.data.Break.Sum,
-                "value2": this.props.data.Break.Count
-            },
-            {
-                "name" : "Meeting Time",
-                "value" : this.props.data.Meeting.Sum,
-                "value2" : this.props.data.Meeting.Count
-            },
-            {
-                "name" : "Work Time",
-                "value" : this.props.data.Work.Sum,
-                "value2" : this.props.data.Work.Count
-            }
-        ]
-        console.log(tooltipData)
+        // const tooltipData=[
+        //     {
+        //         "name" : "Break Time",
+        //         "value" : this.props.data.Break.Sum,
+        //         "value2": this.props.data.Break.Count
+        //     },
+        //     {
+        //         "name" : "Meeting Time",
+        //         "value" : this.props.data.Meeting.Sum,
+        //         "value2" : this.props.data.Meeting.Count
+        //     },
+        //     {
+        //         "name" : "Work Time",
+        //         "value" : this.props.data.Work.Sum,
+        //         "value2" : this.props.data.Work.Count
+        //     }
+        // ]
+        // console.log(tooltipData)
         return (
-            <PieChart width={400} height={400}>
-              <Pie
-                data={chartData}
-                cx={180}
-                cy={200}
-                labelLine={false}
-                outerRadius={100}
-                fill="#8884d8"
-                dataKey="value"
-              >
-                {chartData.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                ))}
-              </Pie>
-              <Tooltip formatter={(value, name, item, index, payload) => {
-                if (name === 'Break'){
-                    value = value + ' minutes (' + tooltipData[0].value2 + ' Entries)'
-                }
-                else if (name == 'Meeting'){
-                    value = value + ' minutes (' + tooltipData[1].value2 + ' Entries)'
-                }
-                else{
-                    value = value + ' minutes (' + tooltipData[2].value2 + ' Entries)'
-                }
-                return [value, name]
-              }} />
-              <Legend className={styles.legend} />
-            </PieChart>
+        <BarChart
+          width={400}
+          height={400}
+          data={chartData}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Break" stackId="a" fill="#8884d8" />
+          <Bar dataKey="Meeting" stackId="a" fill="#82ca9d" />
+          <Bar dataKey="Work" stackId="a" fill="#FFBB28" />
+        </BarChart>
           );
     }
 }
@@ -347,7 +365,7 @@ export default class UserGraphs extends react.Component {
                     })
                 }
             })
-            axios.post('http://picoperformance.centralindia.cloudapp.azure.com:5000/api/stackchart', {
+            axios.post('http://picoperformance.centralindia.cloudapp.azure.com:5000/api/stackChart', {
                 "EmployeeId" : props.user.EmployeeId,
                 "EndDate": props.filter[1],                
             },{
@@ -356,6 +374,7 @@ export default class UserGraphs extends react.Component {
                 }
             }).then((response)=>{
                 if (response.data.success === true){
+                    console.log(response.data.data.Chart)
                     var ldd = this.state.loaded
                     var errd = this.state.er
                     var datadd = this.state.data
@@ -403,7 +422,7 @@ export default class UserGraphs extends react.Component {
                         <PieChartYesterDay data={this.state.data[1]} user={this.props.user} filter={this.state.filter}/>
                     </Grid>
                     <Grid className={styles.graph} item xs={12} lg={4} sx={{textAlign: 'center', width: '100%'}}>
-                        Graph 3
+                        <StackedChart data={this.state.data[2]} user={this.props.user} filter={this.state.filter}/>
                     </Grid>
                 </Grid>
             </>
