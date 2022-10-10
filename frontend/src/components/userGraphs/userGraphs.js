72088fbd27c2d8410d18ea35395687ae8ab457be
Gourav1100 <gauravb1100@gmail.com>
2022-10-10 09:15:25 +0530

    charts fix.

--- frontend/src/components/userGraphs/userGraphs.js
+++ frontend/src/components/userGraphs/userGraphs.js
@@ -8,47 +8,49 @@ class PieChartToday extends react.Component{
     constructor(props){
         super(props);
         this.state = {
-            er: true,
-            data: {}
+            filter: props.filter,
+            data: props.data
+        }
+    }
+    componentWillReceiveProps(props) {
+        if(props.filter != this.state.filter) {
+            this.setState({
+                filter: props.filter,
+                data: props.data
+            })
         }
     }
     render(){
-
-        if (this.state.er){
-            return(
-                <text>Chart Cannot Be displayed!</text>
-            )
-        }
         const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
         const chartData = [
             {
                 "name" : "Break Time",
-                "value" : this.state.data.Break.Sum == null ? 0 : this.state.data.Break.Sum
+                "value" : this.props.data.Break.Sum == null ? 0 : this.props.data.Break.Sum
             },
             {
                 "name" : "Meeting Time",
-                "value" : this.state.data.Meeting.Sum == null ? 0 : this.state.data.Meeting.Sum
+                "value" : this.props.data.Meeting.Sum == null ? 0 : this.props.data.Meeting.Sum
             },
             {
                 "name" : "Work Time",
-                "value" : this.state.data.Work.Sum == null ? 0 : this.state.data.Work.Sum
+                "value" : this.props.data.Work.Sum == null ? 0 : this.props.data.Work.Sum
             }
         ]
         const tooltipData=[
             {
                 "name" : "Break Time",
-                "value" : this.state.data.Break.Sum,
-                "value2": this.state.data.Break.Count
+                "value" : this.props.data.Break.Sum,
+                "value2": this.props.data.Break.Count
             },
             {
                 "name" : "Meeting Time",
-                "value" : this.state.data.Meeting.Sum,
-                "value2" : this.state.data.Meeting.Count
+                "value" : this.props.data.Meeting.Sum,
+                "value2" : this.props.data.Meeting.Count
             },
             {
                 "name" : "Work Time",
-                "value" : this.state.data.Work.Sum,
-                "value2" : this.state.data.Work.Count
+                "value" : this.props.data.Work.Sum,
+                "value2" : this.props.data.Work.Count
             }
         ]
         return (
@@ -82,39 +84,22 @@ class PieChartToday extends react.Component{
             </PieChart>
           );
     }
-    componentDidMount() {
-        axios.post('http://picoperformance.centralindia.cloudapp.azure.com:5000/api/piechart', {
-            "EmployeeId" : this.props.user.EmployeeId,
-            "StartDate" : this.props.filter[0] + ' 00:00:00',
-            "EndDate" : this.props.filter[1] + ' 23:59:59'
-        }, {
-            headers: {
-                Authorization: `Bearer ${this.props.user.token}`
-            }
-        }).then((response)=>{
-            if (response.data.success === true){
-                this.setState({
-                    er: false,
-                    data: response.data.data.Details
-                })
-
-            }
-        }).catch((err) =>{
-            if(err) {
-                this.setState({
-                    er: true,
-                })
-            }
-        })
-    }
 }
 
 class PieChartYesterDay extends react.Component{
     constructor(props){
         super(props);
         this.state = {
-            er: true,
-            data: {}
+            filter: props.filter,
+            data: props.data
+        }
+    }
+    componentWillReceiveProps(props) {
+        if(props.filter != this.state.filter) {
+            this.setState({
+                filter: props.filter,
+                data: props.data
+            })
         }
     }
     render(){
@@ -123,41 +108,36 @@ class PieChartYesterDay extends react.Component{
                 <text>Chart is not availabe when filter spans more than 1 date</text>
             )
         }
-        if (this.state.er){
-            return(
-                <text>Chart Cannot Be displayed!</text>
-            )
-        }
         const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
         const chartData = [
             {
                 "name" : "Break Time",
-                "value" : this.state.data.Break.Sum == null ? 0 : this.state.data.Break.Sum
+                "value" : this.props.data.Break.Sum == null ? 0 : this.props.data.Break.Sum
             },
             {
                 "name" : "Meeting Time",
-                "value" : this.state.data.Meeting.Sum == null ? 0 : this.state.data.Meeting.Sum
+                "value" : this.props.data.Meeting.Sum == null ? 0 : this.props.data.Meeting.Sum
             },
             {
                 "name" : "Work Time",
-                "value" : this.state.data.Work.Sum == null ? 0 : this.state.data.Work.Sum
+                "value" : this.props.data.Work.Sum == null ? 0 : this.props.data.Work.Sum
             }
         ]
         const tooltipData=[
             {
                 "name" : "Break Time",
-                "value" : this.state.data.Break.Sum,
-                "value2": this.state.data.Break.Count
+                "value" : this.props.data.Break.Sum,
+                "value2": this.props.data.Break.Count
             },
             {
                 "name" : "Meeting Time",
-                "value" : this.state.data.Meeting.Sum,
-                "value2" : this.state.data.Meeting.Count
+                "value" : this.props.data.Meeting.Sum,
+                "value2" : this.props.data.Meeting.Count
             },
             {
                 "name" : "Work Time",
-                "value" : this.state.data.Work.Sum,
-                "value2" : this.state.data.Work.Count
+                "value" : this.props.data.Work.Sum,
+                "value2" : this.props.data.Work.Count
             }
         ]
         return (
@@ -190,30 +170,6 @@ class PieChartYesterDay extends react.Component{
               <Legend className={styles.legend} />
             </PieChart>
           );
-    }
-    componentDidMount() {
-        axios.post('http://picoperformance.centralindia.cloudapp.azure.com:5000/api/piechart', {
-            "EmployeeId" : this.props.user.EmployeeId,
-            "StartDate" : this.props.filter[0] + ' 00:00:00',
-            "EndDate": this.props.filter[1] + ' 23:59:59'
-        },{
-            headers: {
-                Authorization: `Bearer ${this.props.user.token}`
-            }
-        }).then((response)=>{
-            if (response.data.success === true){
-                this.setState({
-                    er: false,
-                    data: response.data.data.Details
-                })
-            }
-        }).catch((err) =>{
-            if(err) {
-                this.setState({
-                    er: true,
-                })
-            }
-        })
     }
 }
 
@@ -233,16 +189,64 @@ class PieChartYesterDay extends react.Component{
 export default class UserGraphs extends react.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            loaded: false,
+            er: true,
+            data: {},
+            filter: props.filter
+        }
+    }
+    componentWillReceiveProps(props) {
+        if(this.state.filter != props.filter) {
+            this.setState({
+                loaded: false,
+                err: true,
+                filter: props.filter
+            })
+            axios.post('http://picoperformance.centralindia.cloudapp.azure.com:5000/api/piechart', {
+                "EmployeeId" : props.user.EmployeeId,
+                "StartDate" : props.filter[0] + ' 00:00:00',
+                "EndDate": props.filter[1] + ' 23:59:59'
+            },{
+                headers: {
+                    Authorization: `Bearer ${props.user.token}`
+                }
+            }).then((response)=>{
+                if (response.data.success === true){
+                    this.setState({
+                        loaded: true,
+                        er: false,
+                        data: response.data.data.Details,
+                    });
+                }
+            }).catch((err) =>{
+                if(err) {
+                    this.setState({
+                        loaded: true,
+                        er: true,
+                    })
+                }
+            })
+        }
     }
     render() {
+        if(this.state.loaded === false) {
+            return <>Loading Charts...</>
+        }
+        if (this.state.er){
+            return(
+                <text>Chart Cannot Be displayed!</text>
+            )
+        }
+        // console.log(this.state.data)
         return (
             <>
                 <Grid container maxWidth={true} sx={{display: 'flex'}} alignContent={'center'} alignItems={'center'}>
                     <Grid className={styles.graph} item xs={12} lg={4} sx={{textAlign: 'center', width: '100%'}} >
-                        <PieChartToday user={this.props.user} filter={this.props.filter} />
+                        <PieChartToday data={this.state.data} user={this.props.user} filter={this.state.filter} />
                     </Grid>
                     <Grid className={styles.graph} item xs={12} lg={4} sx={{textAlign: 'center', width: '100%'}}>
-                        <PieChartYesterDay user={this.props.user} filter={this.props.filter}/>
+                        <PieChartYesterDay data={this.state.data} user={this.props.user} filter={this.state.filter}/>
                     </Grid>
                     <Grid className={styles.graph} item xs={12} lg={4} sx={{textAlign: 'center', width: '100%'}}>
                         Graph 3
@@ -250,5 +254,33 @@ export default class UserGraphs extends react.Component {
                 </Grid>
             </>
         );
+    }
+    componentDidMount() {
+        if(this.state.loaded === false) {
+            axios.post('http://picoperformance.centralindia.cloudapp.azure.com:5000/api/piechart', {
+                "EmployeeId" : this.props.user.EmployeeId,
+                "StartDate" : this.props.filter[0] + ' 00:00:00',
+                "EndDate": this.props.filter[1] + ' 23:59:59'
+            },{
+                headers: {
+                    Authorization: `Bearer ${this.props.user.token}`
+                }
+            }).then((response)=>{
+                if (response.data.success === true){
+                    this.setState({
+                        er: false,
+                        data: response.data.data.Details,
+                        loaded: true
+                    })
+                }
+            }).catch((err) =>{
+                if(err) {
+                    this.setState({
+                        er: true,
+                        loaded: true
+                    })
+                }
+            })
+        }
     }
 }
