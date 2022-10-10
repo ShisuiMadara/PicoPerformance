c05440aa0db77cb523f761a13aa31bf522c989bd
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-10-10 09:51:06 +0530

    fix

--- api/block.js
+++ api/block.js
@@ -1,42 +1,42 @@
 const mysql = require('mysql')
-//done
-function handler(req, res) {
-    var con = mysql.createConnection({
-        host: "localhost",
-        user: "pico",
-        password: "password",
-        database: "picoperformance"
-    });
-    req = req.body
-    var blk = '0'
-    var Blocked = 'Unblocked'
-    if(req.Block === true) {
-        blk = '1'
-        Blocked = 'Blocked'
+// done
+function handler (req, res) {
+  const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'pico',
+    password: 'password',
+    database: 'picoperformance'
+  })
+  req = req.body
+  let blk = '0'
+  let Blocked = 'Unblocked'
+  if (req.Block === true) {
+    blk = '1'
+    Blocked = 'Blocked'
+  }
+  console.log(blk)
+  con.connect(async (err) => {
+    if (err) {
+      res.status(400).send({
+        message: 'Datbase Error',
+        success: false
+      })
+      return 0
     }
-    console.log(blk);
-    con.connect(async (err)=>{
-        if (err) {
-            res.status(400).send({
-                message: "Datbase Error",
-                success: false
-            });
-            return 0
-        }
-        var sql = "Update Employee Set IsBlocked=" + blk + " where EmailId='" + req.EmailId + "'";
-        con.query(sql, function (erro, result) {
-            if (erro) {
-                res.status(400).send({
-                    success: false,
-                    message: 'Query Error'
-                });
-                return 0;
-            }
-            res.send({
-                message: 'User ' +  Blocked + ' Successfully!',
-                success: true
-            });
-        });
+    const sql = 'Update Employee Set IsBlocked=' + blk + " where EmailId='" + req.EmailId + "'"
+    con.query(sql, function (erro, result) {
+      if (erro) {
+        res.status(400).send({
+          success: false,
+          message: 'Query Error'
+        })
+        return 0
+      }
+      res.send({
+        message: 'User ' + Blocked + ' Successfully!',
+        success: true
+      })
     })
+  })
 }
-exports.execute = handler;
+exports.execute = handler
