1913f46e11cbf3c6f6147503ff45f72e85a95997
TejaswiBishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-10-10 10:47:39 +0530

    Bugs Removed and Graph Work

--- api/piechart.js
+++ api/piechart.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql')
+const { execute } = require('./getAllTasks')
 
 function handler (req, res) {
   const con = mysql.createConnection({
@@ -8,7 +9,7 @@ function handler (req, res) {
     database: 'picoperformance'
   })
   req = req.body
-  
+
   con.connect(async (err) => {
     if (err) {
       res.status(400).send({
@@ -17,63 +18,68 @@ function handler (req, res) {
       })
       return 0
     }
-    let sql = 'SELECT Count(TaskId) As "Count", Sum(TimeTaken) As "Sum", TaskType FROM Tasks Where EmployeeId="' + req.EmployeeId + '" AND TaskType="Break" AND StartDate BETWEEN "' + req.StartDate + '" AND "' + req.EndDate + '"'
-    con.query(sql, function (erro, result1) {
-      if (erro) {
-        console.log(erro.message)
-        console.log(1)
-        res.status(400).send({
-          message: 'Query Error',
-          success: false
-        })
-        return 0
-      }
-      sql = 'SELECT Count(TaskId) As "Count", Sum(TimeTaken) As "Sum", TaskType FROM Tasks Where EmployeeId="' + req.EmployeeId + '" AND TaskType="Meeting" AND StartDate BETWEEN "' + req.StartDate + '" AND "' + req.EndDate + '"'
-      con.query(sql, function (erro, result2) {
+    var sql = "Select date_add('" + req.StartDate + "', INTERVAL " + (req.Yesterday?'-1':'0') + " DAY) as 'a', date_add('" + req.EndDate + "', INTERVAL " + (req.Yesterday?'-1':'0') + " DAY) as 'b'"
+    con.query(sql, function(err, result){
+      req.StartDate = result[0].a;
+      req.EndDate = result[0].b;
+      sql = 'SELECT Count(TaskId) As "Count", Sum(TimeTaken) As "Sum", TaskType FROM Tasks Where EmployeeId="' + req.EmployeeId + '" AND TaskType="Break" AND StartDate BETWEEN "' + req.StartDate + '" AND "' + req.EndDate + '"'
+      con.query(sql, function (erro, result1) {
         if (erro) {
           console.log(erro.message)
-          console.log(2)
+          console.log(1)
           res.status(400).send({
             message: 'Query Error',
             success: false
           })
           return 0
         }
-        sql = 'SELECT Count(TaskId) As "Count", Sum(TimeTaken) As "Sum", TaskType FROM Tasks Where EmployeeId="' + req.EmployeeId + '" AND TaskType="Work" AND StartDate BETWEEN "' + req.StartDate + '" AND "' + req.EndDate + '"'
-        con.query(sql, function (erro, result3) {
+        sql = 'SELECT Count(TaskId) As "Count", Sum(TimeTaken) As "Sum", TaskType FROM Tasks Where EmployeeId="' + req.EmployeeId + '" AND TaskType="Meeting" AND StartDate BETWEEN "' + req.StartDate + '" AND "' + req.EndDate + '"'
+        con.query(sql, function (erro, result2) {
           if (erro) {
-            console.log(result3)
-            console.log(3)
+            console.log(erro.message)
+            console.log(2)
             res.status(400).send({
               message: 'Query Error',
               success: false
             })
-            con.end()
             return 0
           }
-          res.send({
-            data: {
-              Details: {
-                Break: {
-                  Count: result1[0].Count,
-                  Sum: result1[0].Sum
-                },
-                Meeting: {
-                  Count: result2[0].Count,
-                  Sum: result2[0].Sum
-                },
-                Work: {
-                  Count: result3[0].Count,
-                  Sum: result3[0].Sum
+          sql = 'SELECT Count(TaskId) As "Count", Sum(TimeTaken) As "Sum", TaskType FROM Tasks Where EmployeeId="' + req.EmployeeId + '" AND TaskType="Work" AND StartDate BETWEEN "' + req.StartDate + '" AND "' + req.EndDate + '"'
+          con.query(sql, function (erro, result3) {
+            if (erro) {
+              console.log(result3)
+              console.log(3)
+              res.status(400).send({
+                message: 'Query Error',
+                success: false
+              })
+              con.end()
+              return 0
+            }
+            res.send({
+              data: {
+                Details: {
+                  Break: {
+                    Count: result1[0].Count,
+                    Sum: result1[0].Sum
+                  },
+                  Meeting: {
+                    Count: result2[0].Count,
+                    Sum: result2[0].Sum
+                  },
+                  Work: {
+                    Count: result3[0].Count,
+                    Sum: result3[0].Sum
+                  }
                 }
-              }
-            },
-            success: true
+              },
+              success: true
+            })
+            con.end()
           })
-          con.end()
         })
       })
-    })
+    }) 
   })
 }
 exports.execute = handler
